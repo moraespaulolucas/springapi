2d1446a6fd391ad5558d91b175b5a3ae206299bf
Paulo Lucas Moraes <moraes.paulolucas@gmail.com>
2022-09-18 18:00:41 -0300

    Creating getter to string attribute

--- src/main/java/com/company/springapi/model/Hello.java
+++ src/main/java/com/company/springapi/model/Hello.java
@@ -3,6 +3,10 @@ package com.company.springapi.model;
 public class Hello {
     private final String string;
 
+    public String getString() {
+        return string;
+    }
+
     public Hello(String string) {
         this.string = string;
     }
